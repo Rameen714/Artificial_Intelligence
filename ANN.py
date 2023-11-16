e7530bd7d3bf946284dcfca5ba0ee3fc0b82a6c7
Rameen Amir <rameen.amir.malik@gmail.com>
2023-11-16 11:10:26 +0500

    Car Number Plate Recognition System for Punjab-ANN

---
+++ ANN.py
@@ -0,0 +1,5 @@
+# ---------------------------------------------Car Number Plate Recognition System for Punjab using ANN-----------------------------------------------------
+
+# This is a program to identify Punjab License Plate of a Car using Artificial Neural Network
+# This program take an Image input and outputs the charcetrs on the numebr plate.
+
