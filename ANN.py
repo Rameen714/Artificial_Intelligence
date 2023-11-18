3ca59c0a0539120cf4a399da033dbb52fae85f30
Rameen Amir <rameen.amir.malik@gmail.com>
2023-11-18 20:08:45 +0500

    starting step 3

--- ANN.py
+++ ANN.py
@@ -3,9 +3,15 @@
 # This is a program to identify Punjab License Plate of a Car using Artificial Neural Network
 # This program take an Image input and outputs the charcters on the numebr plate.
 
-#Step 1: Collecting/ Finding Data Set 
-#Step 2: Cleaning Data Set 
-#Step 3: Dividing Data Set into training, validation and testing
-#Step 4: Building Model
-#Step 5: Model Training
-#Step 6: Model Evaluation (implementing matrics to evaluate model performance)
+# Step 1: Collecting/ Finding Data Set 
+# done
+
+# Step 2: Cleaning Data Set 
+# not required
+
+# Step 3: Dividing Data Set into training, validation and testing
+# starting
+
+# Step 4: Building Model
+# Step 5: Model Training
+# Step 6: Model Evaluation (implementing matrics to evaluate model performance)
